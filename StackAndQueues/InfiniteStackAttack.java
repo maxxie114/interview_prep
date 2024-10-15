d02587e5573938ef5ee166cd7b5fb8950001a405
Max Xie <maxxie114@gmail.com>
2024-10-14 20:24:44 -0700

    complete the auto balance stack

--- StackAndQueues/InfiniteStackAttack.java
+++ StackAndQueues/InfiniteStackAttack.java
@@ -44,7 +44,27 @@ public class InfiniteStackAttack {
                     s2.push(temp2.pop());
                 }
                 s1.push(a);
-            }
+            } else if (s2.size() - s1.size() > 1) {
+                // if stack 2 is longer, move half of the elements to stack 1
+                int amountToMove = (s2.size() - s1.size()) / 2;
+                int s2Size = s2.size();
+                for (int i = 0; i < (s2Size - amountToMove); i++) {
+                    temp.push(s2.pop());
+                }
+                while (!s1.isEmpty()) {
+                    temp2.push(s1.pop());
+                }
+                while (!s2.isEmpty()) {
+                    s1.push(s2.pop());
+                }
+                while (!temp.isEmpty()) {
+                    s2.push(temp.pop());
+                }
+                while (!temp2.isEmpty()) {
+                    s1.push(temp2.pop());
+                }
+                s1.push(a);
+            } 
             // if stack 2 is longer, move half of the elements to stack 1
             else {
                 // If the length of the two stacks are equal or off by 1, push to stack 1
@@ -53,26 +73,46 @@ public class InfiniteStackAttack {
         }
     }
 
-    // Push to the back
+    // Push to the back of the deque
     public void pushLast(int a) {
         // Initialize a temp stack
         Stack<Integer> temp = new Stack<>();
+        Stack<Integer> temp2 = new Stack<>();
         // First check if stack 1 and stack 2 are balanced, if balanced, push to stack 2
-        // The length of the two stack can be off by 1 to account for odd number of elements
-        if ((s1.size() - s2.size()) <= 1 && (s1.size() - s2.size()) >= -1) {
+        if ((s2.size() - s1.size()) <= 1 && (s2.size() - s1.size()) >= -1) {
             s2.push(a);
         }
-        // if stack 2 and stack 1 is not balanced, use another stack to move to lower half part of the stack to the other stack
+        // if stack 1 and stack 2 are not balanced, use another stack to move to lower half part of the stack to the other stack
         else {
-            // if stack 1 is longer, move half of the elements to stack 2
-            if (s1.size() - s2.size() > 1) {
+            // if stack 2 is longer, move half of the elements to stack 1
+            if (s2.size() - s1.size() > 1) {
+                int amountToMove = (s2.size() - s1.size()) / 2;
+                int s2Size = s2.size();
+                for (int i = 0; i < (s2Size - amountToMove); i++) {
+                    temp.push(s2.pop());
+                }
+                while (!s1.isEmpty()) {
+                    temp2.push(s1.pop());
+                }
+                while (!s2.isEmpty()) {
+                    s1.push(s2.pop());
+                }
+                while (!temp.isEmpty()) {
+                    s2.push(temp.pop());
+                }
+                while (!temp2.isEmpty()) {
+                    s1.push(temp2.pop());
+                }
+                s2.push(a);
+            } else if (s1.size() - s2.size() > 1) {
+                // if stack 1 is longer, move half of the elements to stack 2
                 int amountToMove = (s1.size() - s2.size()) / 2;
                 int s1Size = s1.size();
                 for (int i = 0; i < (s1Size - amountToMove); i++) {
                     temp.push(s1.pop());
                 }
                 while (!s2.isEmpty()) {
-                    temp.push(s2.pop());
+                    temp2.push(s2.pop());
                 }
                 while (!s1.isEmpty()) {
                     s2.push(s1.pop());
@@ -80,12 +120,11 @@ public class InfiniteStackAttack {
                 while (!temp.isEmpty()) {
                     s1.push(temp.pop());
                 }
+                while (!temp2.isEmpty()) {
+                    s2.push(temp2.pop());
+                }
                 s2.push(a);
-            }
-            else {
-                // If the length of the two stacks are equal or off by 1, push to stack 2
-                s2.push(a);
-            }
+            } 
         }
     }
 
@@ -108,11 +147,11 @@ public class InfiniteStackAttack {
         Stack<Integer> s1 = new Stack<>();
         Stack<Integer> s2 = new Stack<>();
         // First push 20 elements into stack 1, from 1 to 20
-        for (int i = 20; i > 0; i--) {
+        for (int i = 10; i > 0; i--) {
             s1.push(i);
         }
         // Then push 10 elements into stack 2, from 21 to 30
-        for (int i = 21; i < 31; i++) {
+        for (int i = 11; i < 31; i++) {
             s2.push(i);
         }
         // Initialize the deque
@@ -121,12 +160,12 @@ public class InfiniteStackAttack {
         // Print the deque
         deque.printStacks();
 
-        // Push 0 to the front of the deque, and print the deque
-        deque.pushFirst(0);
-        deque.printStacks();
-
         // Push 32 to the back of the deque, and print the deque
         deque.pushLast(31);
+        deque.printStacks();
+
+        // Push 0 to the front of the deque, and print the deque
+        deque.pushFirst(0);
         deque.printStacks();
     }
 }

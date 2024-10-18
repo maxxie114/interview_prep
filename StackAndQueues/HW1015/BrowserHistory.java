95e8df7dd4d098fa420ec350a129b6742f4664fb
maxxie114 <maxxie114@gmail.com>
2024-10-18 11:09:56 -0700

    upload code for leetcode 128

---
+++ StackAndQueues/HW1015/BrowserHistory.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class BrowserHistory {
+    ArrayDeque<String> history = new ArrayDeque<>();
+
+    public BrowserHistory(String homepage) {
+        
+    }
+    
+    public void visit(String url) {
+        
+    }
+    
+    public String back(int steps) {
+        
+    }
+    
+    public String forward(int steps) {
+        
+    }
+}
